37c6ac1e17180b7099873e40f396bae1287725c6
AndreLuis117 <andreluis2006@outlook.com>
2019-08-05 20:21:28 -0300

    Classes da pilha baseado em arranjos

---
+++ stack-min-project/src/br/univille/estd/stacks/FullStackException.java
@@ -0,0 +1,8 @@
+package br.univille.estd.stacks;
+
+public class FullStackException extends RuntimeException{
+	
+	public FullStackException(String error) {
+		super(error);
+	}
+}
--- stack-min-project/src/br/univille/estd/stacks/StackMin.java
+++ stack-min-project/src/br/univille/estd/stacks/StackMin.java
@@ -1,35 +1,55 @@
-package br.univille.estd.stacks;
+ package br.univille.estd.stacks;
 
 public class StackMin<E> implements Stack<E> {
+	
+	protected int capacity;
+	public static final int CAPACITY = 1000;
+	protected E S[];
+	protected int top = -1;
+	public StackMin() {
+		this(CAPACITY);
+	}
+	public StackMin(int capacity) {
+		this.capacity = capacity;
+		S = (E[])new Object[this.capacity];
+	}
 
+	
 	@Override
 	public int size() {
-		// TODO Auto-generated method stub
-		return -1;
+		return top-1;
 	}
 
 	@Override
 	public boolean isEmpty() {
-		// TODO Auto-generated method stub
-		return false;
+		return top < 0;
 	}
 
 	@Override
 	public E top() throws EmptyStackException {
-		// TODO Auto-generated method stub
-		return null;
+		if(isEmpty()) {
+			throw new EmptyStackException(null);
+		}
+		return S[top];
 	}
 
 	@Override
 	public void push(E element) {
-		// TODO Auto-generated method stub
-		
+		if(size() == capacity) {
+			throw new FullStackException("A pilha está cheia");
+		}
+		top = top + 1;
+		S[top] = element;
 	}
 
 	@Override
 	public E pop() throws EmptyStackException {
-		// TODO Auto-generated method stub
-		return null;
+		if(isEmpty()) {
+			throw new EmptyStackException("A pilha está vazia");
+		}
+		E element = S[top];
+		top = top -1;
+		return element;
 	}
 
 	@Override
